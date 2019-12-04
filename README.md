2b9b867f8a4ebe89cc4eaba533778178d2686738
medrectechsumit <sumit@medrectechnologies.com>
2019-12-04 11:46:42 +0530

    react test

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">React & Redux Todo App</h1>
+
+<p align="center">
+  <img src="https://github.com/lucasbento/react-redux-todo/raw/master/content/todo.gif">
+</p>
+
+## Running
+
+```sh
+npm start
+```
+
+### Production build
+
+```sh
+npm run build # build files will be available in ./dist
+```
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import TodoTextField from './TodoTextField';
+
+class Header extends Component {
+  render() {
+
+    const { addTodo } = this.props.actions;
+
+    return (
+      <div>
+        <TodoTextField onSubmit={text => addTodo(text)} />
+      </div>
+    );
+  }
+}
+
+export default Header;
